289878d23f6541f9478860a9eb5938259c112864
Kent C. Dodds <me+github@kentcdodds.com>
2022-06-21 11:06:31 -0600

    exercises/04-authentication

--- src/utils/api-client.exercise.js
+++ src/utils/api-client.exercise.js
@@ -1,19 +1,19 @@
+const apiURL = process.env.REACT_APP_API_URL
+
 function client(endpoint, customConfig = {}) {
   const config = {
     method: 'GET',
     ...customConfig,
   }
 
-  return window
-    .fetch(`${process.env.REACT_APP_API_URL}/${endpoint}`, config)
-    .then(async response => {
-      const data = await response.json()
-      if (response.ok) {
-        return data
-      } else {
-        return Promise.reject(data)
-      }
-    })
+  return window.fetch(`${apiURL}/${endpoint}`, config).then(async response => {
+    const data = await response.json()
+    if (response.ok) {
+      return data
+    } else {
+      return Promise.reject(data)
+    }
+  })
 }
 
 export {client}

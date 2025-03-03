34356c1d3065554a42db57e46d6ae85a11d9ff1f
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-03-03 17:02:58 -0300

    feat: implement user retrieval and authorization in App component

--- src/app.exercise.js
+++ src/app.exercise.js
@@ -3,11 +3,26 @@ import {jsx} from '@emotion/core'
 
 import * as React from 'react'
 import * as auth from 'auth-provider'
+import {client} from 'utils/api-client'
 import {AuthenticatedApp} from './authenticated-app'
 import {UnauthenticatedApp} from './unauthenticated-app'
 
+async function getUser() {
+  let user = null
+  const token = await auth.getToken()
+  if (token) {
+    const data = await client('me', {token})
+    user = data.user
+  }
+  return user
+}
+
 function App() {
   const [user, setUser] = React.useState(null)
+
+  React.useEffect(() => {
+    getUser().then(u => setUser(u))
+  }, [])
 
   const login = form => auth.login(form).then(u => setUser(u))
   const register = form => auth.register(form).then(u => setUser(u))

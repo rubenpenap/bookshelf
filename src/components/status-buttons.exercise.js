775bef62305933b6954ee0f582f93c60031a62c6
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-03-05 16:39:02 -0300

    feat: enhance list item mutation functions with customizable options and improve error handling in TooltipButton

--- src/components/status-buttons.exercise.js
+++ src/components/status-buttons.exercise.js
@@ -21,10 +21,14 @@ import {useAsync} from 'utils/hooks'
 import {CircleButton, Spinner} from './lib'
 
 function TooltipButton({label, highlight, onClick, icon, ...rest}) {
-  const {isLoading, isError, error, run} = useAsync()
+  const {isLoading, isError, error, run, reset} = useAsync()
 
   function handleClick() {
-    run(onClick())
+    if (isError) {
+      reset()
+    } else {
+      run(onClick())
+    }
   }
 
   return (
@@ -54,9 +58,9 @@ function TooltipButton({label, highlight, onClick, icon, ...rest}) {
 function StatusButtons({user, book}) {
   const listItem = useListItem(user, book.id)
 
-  const [update] = useUpdateListItem(user)
-  const [remove] = useRemoveListItem(user)
-  const [create] = useCreateListItem(user)
+  const [update] = useUpdateListItem(user, {throwOnError: true})
+  const [remove] = useRemoveListItem(user, {throwOnError: true})
+  const [create] = useCreateListItem(user, {throwOnError: true})
 
   return (
     <React.Fragment>

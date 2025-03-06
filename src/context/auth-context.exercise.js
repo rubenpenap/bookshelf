9e15bd6ce964bc7b61ca8625f746a71fc53a65f7
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-03-06 12:19:11 -0300

    feat: implement AuthContext for authentication management in the app

--- src/context/auth-context.exercise.js
+++ src/context/auth-context.exercise.js
@@ -1,2 +1,5 @@
-// 🐨 create and export a React context variable for the AuthContext
-// 💰 using React.createContext
+import React from 'react'
+
+const AuthContext = React.createContext()
+
+export {AuthContext}

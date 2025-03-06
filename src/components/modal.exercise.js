f7b0774091dc7f6c5ad2137794d9082ea29e40b8
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-03-06 18:34:45 -0300

    refactor: integrate callAll utility in Modal button click handlers

--- src/components/modal.exercise.js
+++ src/components/modal.exercise.js
@@ -4,6 +4,8 @@ import {jsx} from '@emotion/core'
 import * as React from 'react'
 import {Dialog} from './lib'
 
+const callAll = (...fns) => (...args) => fns.forEach(fn => fn && fn(...args))
+
 const ModalContext = React.createContext()
 
 function Modal(props) {
@@ -15,14 +17,14 @@ function Modal(props) {
 function ModalDismissButton({children: child}) {
   const [, setIsOpen] = React.useContext(ModalContext)
   return React.cloneElement(child, {
-    onClick: () => setIsOpen(false),
+    onClick: callAll(() => setIsOpen(false), child.props.onClick),
   })
 }
 
 function ModalOpenButton({children: child}) {
   const [, setIsOpen] = React.useContext(ModalContext)
   return React.cloneElement(child, {
-    onClick: () => setIsOpen(true),
+    onClick: callAll(() => setIsOpen(true), child.props.onClick),
   })
 }
 

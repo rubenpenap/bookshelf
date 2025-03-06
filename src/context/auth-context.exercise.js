612a9ac20ef820f5e8b71149ae5982fcb90303f3
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-03-06 15:36:45 -0300

    refactor: implement AuthProvider for centralized authentication management and simplify App component

--- src/context/auth-context.exercise.js
+++ src/context/auth-context.exercise.js
@@ -1,13 +1,83 @@
 import * as React from 'react'
+import {queryCache} from 'react-query'
+import * as auth from 'auth-provider'
+import {FullPageSpinner} from 'components/lib'
+import * as colors from 'styles/colors'
+import {client} from 'utils/api-client'
+import {useAsync} from 'utils/hooks'
 
 const AuthContext = React.createContext()
+
+async function getUser() {
+  let user = null
+
+  const token = await auth.getToken()
+  if (token) {
+    const data = await client('me', {token})
+    user = data.user
+  }
+
+  return user
+}
+
+function AuthProvider(props) {
+  const {
+    data: user,
+    error,
+    isLoading,
+    isIdle,
+    isError,
+    isSuccess,
+    run,
+    setData,
+  } = useAsync()
+
+  React.useEffect(() => {
+    run(getUser())
+  }, [run])
+
+  const login = form => auth.login(form).then(user => setData(user))
+  const register = form => auth.register(form).then(user => setData(user))
+  const logout = () => {
+    auth.logout()
+    queryCache.clear()
+    setData(null)
+  }
+
+  if (isLoading || isIdle) {
+    return <FullPageSpinner />
+  }
+
+  if (isError) {
+    return (
+      <div
+        css={{
+          color: colors.danger,
+          height: '100vh',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <p>Uh oh... There's a problem. Try refreshing the app.</p>
+        <pre>{error.message}</pre>
+      </div>
+    )
+  }
+
+  if (isSuccess) {
+    const value = {user, login, register, logout}
+    return <AuthContext.Provider value={value} {...props} />
+  }
+}
 
 function useAuth() {
   const context = React.useContext(AuthContext)
   if (context === undefined) {
-    throw new Error(`useAuth must be used within a AuthContext provider`)
+    throw new Error(`useAuth must be used within an AuthProvider`)
   }
   return context
 }
 
-export {AuthContext, useAuth}
+export {AuthProvider, useAuth}

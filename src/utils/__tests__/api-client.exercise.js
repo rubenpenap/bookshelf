bbf78b0e8ca77820e32d078d6ac7c73d3c14c083
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-03-07 23:17:43 -0300

    test: enhance API client tests for GET requests and token authentication

--- src/utils/__tests__/api-client.exercise.js
+++ src/utils/__tests__/api-client.exercise.js
@@ -2,14 +2,14 @@ import {server, rest} from 'test/server'
 import {client} from 'utils/api-client'
 
 const apiURL = process.env.REACT_APP_API_URL
+const endpoint = 'test-endpoint'
+const mockResult = {mockValue: 'VALUE'}
 
 beforeAll(() => server.listen())
 afterAll(() => server.close())
 afterEach(() => server.resetHandlers())
 
 test('calls fetch at the endpoint with the arguments for GET requests', async () => {
-  const endpoint = 'test-endpoint'
-  const mockResult = {mockValue: 'VALUE'}
   server.use(
     rest.get(`${apiURL}/${endpoint}`, async (req, res, ctx) => {
       return res(ctx.json(mockResult))
@@ -19,19 +19,18 @@ test('calls fetch at the endpoint with the arguments for GET requests', async ()
   expect(result).toEqual(mockResult)
 })
 
-test('adds auth token when a token is provided', async () => {})
-// 🐨 create a fake token (it can be set to any string you want)
-// 🐨 create a "request" variable with let
-// 🐨 create a server handler to handle a test request you'll be making
-// 🐨 inside the server handler, assign "request" to "req" so we can use that
-//     to assert things later.
-//     💰 so, something like...
-//       async (req, res, ctx) => {
-//         request = req
-//         ... etc...
-//
-// 🐨 call the client with the token (note that it's async)
-// 🐨 verify that `request.headers.get('Authorization')` is correct (it should include the token)
+test('adds auth token when a token is provided', async () => {
+  const token = 'FAKE_TOKEN'
+  let request
+  server.use(
+    rest.get(`${apiURL}/${endpoint}`, async (req, res, ctx) => {
+      request = req
+      return res(ctx.json(mockResult))
+    }),
+  )
+  await client(endpoint, {token})
+  expect(request.headers.get('Authorization')).toBe(`Bearer ${token}`)
+})
 
 test('allows for config overrides', async () => {})
 // 🐨 do a very similar setup to the previous test

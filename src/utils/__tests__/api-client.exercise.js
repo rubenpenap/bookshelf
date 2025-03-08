e549ecbc674ecaa1d868972bb4161f2ea1093b69
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-03-07 23:13:26 -0300

    test: implement API client tests for GET requests and token handling

--- src/utils/__tests__/api-client.exercise.js
+++ src/utils/__tests__/api-client.exercise.js
@@ -1,31 +1,25 @@
-// 🐨 you'll need the test server
-// 💰 the way that our tests are set up, you'll find this in `src/test/server/test-server.js`
-// import {server, rest} from 'test/server'
-// 🐨 grab the client
-// import {client} from '../api-client'
+import {server, rest} from 'test/server'
+import {client} from 'utils/api-client'
 
-// 🐨 add a beforeAll to start the server with `server.listen()`
-// 🐨 add an afterAll to stop the server when `server.close()`
-// 🐨 afterEach test, reset the server handlers to their original handlers
-// via `server.resetHandlers()`
+const apiURL = process.env.REACT_APP_API_URL
 
-// 🐨 flesh these out:
+beforeAll(() => server.listen())
+afterAll(() => server.close())
+afterEach(() => server.resetHandlers())
 
-test.todo('calls fetch at the endpoint with the arguments for GET requests')
-// 🐨 add a server handler to handle a test request you'll be making
-// 💰 because this is the first one, I'll give you the code for how to do that.
-// const endpoint = 'test-endpoint'
-// const mockResult = {mockValue: 'VALUE'}
-// server.use(
-//   rest.get(`${apiURL}/${endpoint}`, async (req, res, ctx) => {
-//     return res(ctx.json(mockResult))
-//   }),
-// )
-//
-// 🐨 call the client (don't forget that it's asynchronous)
-// 🐨 assert that the resolved value from the client call is correct
+test('calls fetch at the endpoint with the arguments for GET requests', async () => {
+  const endpoint = 'test-endpoint'
+  const mockResult = {mockValue: 'VALUE'}
+  server.use(
+    rest.get(`${apiURL}/${endpoint}`, async (req, res, ctx) => {
+      return res(ctx.json(mockResult))
+    }),
+  )
+  const result = await client(endpoint)
+  expect(result).toEqual(mockResult)
+})
 
-test.todo('adds auth token when a token is provided')
+test('adds auth token when a token is provided', async () => {})
 // 🐨 create a fake token (it can be set to any string you want)
 // 🐨 create a "request" variable with let
 // 🐨 create a server handler to handle a test request you'll be making
@@ -39,15 +33,13 @@ test.todo('adds auth token when a token is provided')
 // 🐨 call the client with the token (note that it's async)
 // 🐨 verify that `request.headers.get('Authorization')` is correct (it should include the token)
 
-test.todo('allows for config overrides')
+test('allows for config overrides', async () => {})
 // 🐨 do a very similar setup to the previous test
 // 🐨 create a custom config that specifies properties like "mode" of "cors" and a custom header
 // 🐨 call the client with the endpoint and the custom config
 // 🐨 verify the request had the correct properties
 
-test.todo(
-  'when data is provided, it is stringified and the method defaults to POST',
-)
+test('when data is provided, it is stringified and the method defaults to POST', async () => {})
 // 🐨 create a mock data object
 // 🐨 create a server handler very similar to the previous ones to handle the post request
 //    💰 Use rest.post instead of rest.get like we've been doing so far

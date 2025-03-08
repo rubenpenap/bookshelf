53b6b5c66041d20e73b40b62ba4d8bb24a253b48
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-03-07 23:50:53 -0300

    test: refactor API client tests for consistency and clarity

--- src/utils/__tests__/api-client.exercise.js
+++ src/utils/__tests__/api-client.exercise.js
@@ -1,58 +1,78 @@
 import {server, rest} from 'test/server'
-import {client} from 'utils/api-client'
+import {client} from '../api-client'
 
 const apiURL = process.env.REACT_APP_API_URL
-const endpoint = 'test-endpoint'
-const mockResult = {mockValue: 'VALUE'}
 
+// enable API mocking in test runs using the same request handlers
+// as for the client-side mocking.
 beforeAll(() => server.listen())
 afterAll(() => server.close())
 afterEach(() => server.resetHandlers())
 
-test('calls fetch at the endpoint with the arguments for GET requests', async () => {
+test('makes GET requests to the given endpoint', async () => {
+  const endpoint = 'test-endpoint'
+  const mockResult = {mockValue: 'VALUE'}
   server.use(
     rest.get(`${apiURL}/${endpoint}`, async (req, res, ctx) => {
       return res(ctx.json(mockResult))
     }),
   )
+
   const result = await client(endpoint)
+
   expect(result).toEqual(mockResult)
 })
 
 test('adds auth token when a token is provided', async () => {
   const token = 'FAKE_TOKEN'
+
   let request
+  const endpoint = 'test-endpoint'
+  const mockResult = {mockValue: 'VALUE'}
   server.use(
     rest.get(`${apiURL}/${endpoint}`, async (req, res, ctx) => {
       request = req
       return res(ctx.json(mockResult))
     }),
   )
+
   await client(endpoint, {token})
+
   expect(request.headers.get('Authorization')).toBe(`Bearer ${token}`)
 })
 
 test('allows for config overrides', async () => {
-  const mode = 'cors'
-  const customConfig = {mode: mode}
   let request
+  const endpoint = 'test-endpoint'
+  const mockResult = {mockValue: 'VALUE'}
   server.use(
-    rest.get(`${apiURL}/${endpoint}`, async (req, res, ctx) => {
+    rest.put(`${apiURL}/${endpoint}`, async (req, res, ctx) => {
       request = req
       return res(ctx.json(mockResult))
     }),
   )
+
+  const customConfig = {
+    method: 'PUT',
+    headers: {'Content-Type': 'fake-type'},
+  }
+
   await client(endpoint, customConfig)
-  expect(request.mode).toBe(mode)
+
+  expect(request.headers.get('Content-Type')).toBe(
+    customConfig.headers['Content-Type'],
+  )
 })
 
 test('when data is provided, it is stringified and the method defaults to POST', async () => {
-  const data = {a: 'b'}
+  const endpoint = 'test-endpoint'
   server.use(
     rest.post(`${apiURL}/${endpoint}`, async (req, res, ctx) => {
       return res(ctx.json(req.body))
     }),
   )
+  const data = {a: 'b'}
   const result = await client(endpoint, {data})
+
   expect(result).toEqual(data)
 })
--- src/utils/__tests__/misc.exercise.js
+++ src/utils/__tests__/misc.exercise.js
@@ -1,4 +1,4 @@
-import {formatDate} from 'utils/misc'
+import {formatDate} from '../misc'
 
 test('formatDate formats the date to look nice', () => {
   expect(formatDate(new Date('October 18, 1988'))).toBe('Oct 88')

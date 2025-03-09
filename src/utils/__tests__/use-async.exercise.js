eb126d5c98424b63a3e7c1648c0dc8d2e8f351dd
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-03-08 21:04:45 -0300

    test: implement initial tests for useAsync hook

--- src/utils/__tests__/use-async.exercise.js
+++ src/utils/__tests__/use-async.exercise.js
@@ -1,10 +1,6 @@
-// 🐨 We'll use renderHook rather than render here
-// import {renderHook, act} from '@testing-library/react'
-// 🐨 Here's the thing you'll be testing:
-// import {useAsync} from '../hooks'
+import {renderHook, act} from '@testing-library/react'
+import {useAsync} from '../hooks'
 
-// 💰 I'm going to give this to you. It's a way for you to create a promise
-// which you can imperatively resolve or reject whenever you want.
 // function deferred() {
 //   let resolve, reject
 //   const promise = new Promise((res, rej) => {
@@ -22,11 +18,22 @@
 // await promise
 // do stuff/make assertions you want to after the promise has resolved
 
-// 🐨 flesh out these tests
-test.todo('calling run with a promise which resolves')
-// 🐨 get a promise and resolve function from the deferred utility
-// 🐨 use renderHook with useAsync to get the result
-// 🐨 assert the result.current is the correct default state
+test('calling run with a promise which resolves', async () => {
+  const {result} = renderHook(() => useAsync())
+  expect(result.current).toEqual({
+    isIdle: true,
+    isLoading: false,
+    isError: false,
+    isSuccess: false,
+    setData: expect.any(Function),
+    setError: expect.any(Function),
+    error: null,
+    status: 'idle',
+    data: null,
+    run: expect.any(Function),
+    reset: expect.any(Function),
+  })
+})
 
 // 🐨 call `run`, passing the promise
 //    (💰 this updates state so it needs to be done in an `act` callback)
@@ -39,23 +46,23 @@ test.todo('calling run with a promise which resolves')
 // 🐨 call `reset` (💰 this will update state, so...)
 // 🐨 assert the result.current has actually been reset
 
-test.todo('calling run with a promise which rejects')
+test('calling run with a promise which rejects', async () => {})
 // 🐨 this will be very similar to the previous test, except you'll reject the
 // promise instead and assert on the error state.
 // 💰 to avoid the promise actually failing your test, you can catch
 //    the promise returned from `run` with `.catch(() => {})`
 
-test.todo('can specify an initial state')
+test('can specify an initial state', async () => {})
 // 💰 useAsync(customInitialState)
 
-test.todo('can set the data')
+test('can set the data', async () => {})
 // 💰 result.current.setData('whatever you want')
 
-test.todo('can set the error')
+test('can set the error', async () => {})
 // 💰 result.current.setError('whatever you want')
 
-test.todo('No state updates happen if the component is unmounted while pending')
+test('No state updates happen if the component is unmounted while pending', async () => {})
 // 💰 const {result, unmount} = renderHook(...)
 // 🐨 ensure that console.error is not called (React will call console.error if updates happen when unmounted)
 
-test.todo('calling "run" without a promise results in an early error')
+test('calling "run" without a promise results in an early error', async () => {})

af160507295f53657365d9228f31e43400791a56
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-03-09 13:53:00 -0300

    test: enhance book screen exercise test with user and book data mocking

--- src/__tests__/book-screen.exercise.js
+++ src/__tests__/book-screen.exercise.js
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import {render, screen, waitForElementToBeRemoved} from '@testing-library/react'
 import {queryCache} from 'react-query'
-// import {buildUser, buildBook} from 'test/generate'
+import {buildUser, buildBook} from 'test/generate'
 import * as auth from 'auth-provider'
 import {AppProviders} from 'context'
 import {App} from 'app'
@@ -12,18 +12,33 @@ afterEach(() => {
 })
 
 test('renders all the book information', async () => {
+  const user = buildUser()
+  const book = buildBook()
   window.localStorage.setItem(auth.localStorageKey, 'SOME_FAKE_TOKEN')
+  window.history.pushState({}, 'Test page', `/book/${book.id}`)
   const originalFetch = window.fetch
   window.fetch = async (url, config) => {
     if (url.endsWith('/bootstrap')) {
       return {
         ok: true,
-        json: async () => ({user: {username: 'Bob'}, listItems: []}),
+        json: async () => ({
+          user: {...user, token: 'SOME_FAKE_TOKEN'},
+          listItems: [],
+        }),
+      }
+    } else if (url.endsWith(`/books/${book.id}`)) {
+      return {
+        ok: true,
+        json: async () => ({book}),
       }
     }
+    console.log(url, config)
     return originalFetch(url, config)
   }
   render(<App />, {wrapper: AppProviders})
   await waitForElementToBeRemoved(() => screen.getByLabelText(/loading/i))
+  await waitForElementToBeRemoved(() =>
+    screen.getByRole('heading', {name: /loading/i}),
+  )
   screen.debug()
 })

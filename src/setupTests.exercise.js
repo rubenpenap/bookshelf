dd1239e4256eef73ad7b082f1db082ea52d407b9
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-03-09 16:31:31 -0300

    test: refactor book screen tests to use custom render function and improve loading state handling

--- src/setupTests.exercise.js
+++ src/setupTests.exercise.js
@@ -1,8 +1,22 @@
 import '@testing-library/jest-dom'
+import {queryCache} from 'react-query'
 import {server} from 'test/server'
+import * as auth from 'auth-provider'
+import * as usersDB from 'test/data/users'
+import * as booksDB from 'test/data/books'
+import * as listItemsDB from 'test/data/list-items'
 
 // enable API mocking in test runs using the same request handlers
 // as for the client-side mocking.
 beforeAll(() => server.listen())
 afterAll(() => server.close())
-afterEach(() => server.resetHandlers())
+afterEach(async () => {
+  server.resetHandlers()
+  queryCache.clear()
+  await Promise.all([
+    auth.logout(),
+    usersDB.reset(),
+    booksDB.reset(),
+    listItemsDB.reset(),
+  ])
+})

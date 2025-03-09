240733078b4b8ee7f7b61c2163fe818e04c50056
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-03-09 20:49:37 -0300

    test: enhance smoke.exercise with additional interactions and validations

--- cypress/e2e/smoke.exercise.js
+++ cypress/e2e/smoke.exercise.js
@@ -32,5 +32,23 @@ describe('smoke', () => {
       cy.findAllByRole('listitem').should('have.length', 1)
       cy.findByRole('link', {name: /voice of war/i}).click()
     })
+
+    cy.findByRole('textbox', {name: /notes/i}).type('This is an awesome book')
+    cy.findByLabelText(/loading/i).should('exist')
+    cy.findByLabelText(/loading/i).should('not.exist')
+
+    cy.findByRole('button', {name: /mark as read/i}).click()
+
+    cy.findByRole('radio', {name: /5 stars/i}).click({force: true})
+
+    cy.findByRole('navigation').within(() => {
+      cy.findByRole('link', {name: /finished books/i}).click()
+    })
+
+    cy.findByRole('main').within(() => {
+      cy.findAllByRole('listitem').should('have.length', 1)
+      cy.findByRole('radio', {name: /5 stars/i}).should('be.checked')
+      cy.findByRole('link', {name: /voice of war/i}).click()
+    })
   })
 })

5b6ec576898579efa9947d15c554fcf5f5e9aa92
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-03-09 20:43:06 -0300

    test: enhance smoke.exercise with navigation and search functionality

--- cypress/e2e/smoke.exercise.js
+++ cypress/e2e/smoke.exercise.js
@@ -12,5 +12,25 @@ describe('smoke', () => {
       cy.findByLabelText(/password/i).type(user.password)
       cy.findByRole('button', {name: /register/i}).click()
     })
+
+    cy.findByRole('navigation').within(() => {
+      cy.findByRole('link', {name: /discover/i}).click()
+    })
+
+    cy.findByRole('main').within(() => {
+      cy.findByRole('searchbox', {name: /search/i}).type('Voice of war{enter}')
+      cy.findByRole('listitem', {name: /voice of war/i}).within(() => {
+        cy.findByRole('button', {name: /add to list/i}).click()
+      })
+    })
+
+    cy.findByRole('navigation').within(() => {
+      cy.findByRole('link', {name: /reading list/i}).click()
+    })
+
+    cy.findByRole('main').within(() => {
+      cy.findAllByRole('listitem').should('have.length', 1)
+      cy.findByRole('link', {name: /voice of war/i}).click()
+    })
   })
 })

a9a2c433881c866323e354a6c62681a8369c3a0f
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-03-09 19:38:27 -0300

    test: add error handling for book loading and note update failures in book screen tests

--- src/__tests__/book-screen.exercise.js
+++ src/__tests__/book-screen.exercise.js
@@ -7,11 +7,14 @@ import {
   loginAsUser,
 } from 'test/app-test-utils'
 import faker from 'faker'
+import {server, rest} from 'test/server'
 import {buildBook, buildListItem} from 'test/generate'
 import * as booksDB from 'test/data/books'
 import * as listItemsDB from 'test/data/list-items'
 import {formatDate} from 'utils/misc'
 import {App} from 'app'
+
+const apiURL = process.env.REACT_APP_API_URL
 
 const fakeTimerUserEvent = userEvent.setup({
   advanceTimers: () => jest.runOnlyPendingTimers(),
@@ -158,9 +161,58 @@ test('can edit a note', async () => {
   // wait for the loading spinner to go away
   await waitForLoadingToFinish()
 
-  expect(notesTextarea).toHaveValue(newNotes)
+  expect(notesTextarea.value).toBe(newNotes)
 
   expect(await listItemsDB.read(listItem.id)).toMatchObject({
     notes: newNotes,
+  })
+})
+
+describe('console errors', () => {
+  beforeAll(() => {
+    jest.spyOn(console, 'error').mockImplementation(() => {})
+  })
+
+  afterAll(() => {
+    console.error.mockRestore()
+  })
+
+  test('shows an error message when the book fails to load', async () => {
+    const book = {id: 'BAD_ID'}
+    await renderBookScreen({listItem: null, book})
+
+    expect(
+      (await screen.findByRole('alert')).textContent,
+    ).toMatchInlineSnapshot(`"There was an error: Book not found"`)
+    expect(console.error).toHaveBeenCalled()
+  })
+
+  test('note update failures are displayed', async () => {
+    // using fake timers to skip debounce time
+    jest.useFakeTimers()
+    await renderBookScreen()
+
+    const newNotes = faker.lorem.words()
+    const notesTextarea = screen.getByRole('textbox', {name: /notes/i})
+
+    const testErrorMessage = '__test_error_message__'
+    server.use(
+      rest.put(`${apiURL}/list-items/:listItemId`, async (req, res, ctx) => {
+        return res(
+          ctx.status(400),
+          ctx.json({status: 400, message: testErrorMessage}),
+        )
+      }),
+    )
+
+    await fakeTimerUserEvent.type(notesTextarea, newNotes)
+    // wait for the loading spinner to show up
+    await screen.findByLabelText(/loading/i)
+    // wait for the loading spinner to go away
+    await waitForLoadingToFinish()
+
+    expect(screen.getByRole('alert').textContent).toMatchInlineSnapshot(
+      `"There was an error: __test_error_message__"`,
+    )
   })
 })
